8755928bc754c1892d2467f8004ccf98bc984d8c
CyberSec <kali@kali>
2023-05-18 05:08:36 +0100

    cybsec

---
+++ script.js
@@ -0,0 +1,7 @@
+function openMenu() {
+    document.getElementById('sideMenu').style.display = 'block';
+}
+
+function closeMenu() {
+    document.getElementById('sideMenu').style.display = 'none';
+}
